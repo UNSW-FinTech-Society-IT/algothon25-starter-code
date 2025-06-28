90c7b4dbca642a94432e8c71e876cdb15f263210
Eric Liu <eric.m.liu@student.unsw.edu.au>
2025-06-29 00:15:58 +1000

    Stop-loss and take-profit in bollinger_and_rsi.py BUGGY??

--- bollinger_and_rsi.py
+++ bollinger_and_rsi.py
@@ -13,7 +13,7 @@ stock_prices = data[:, 0]
 #  - sd
 #  - three bands
 #  - middle band is moving average
-#  - upper and lower bands represent SD
+#  - upper and lower bands represent SD * multiplier
 
 
 
@@ -26,6 +26,18 @@ class Bollinger_RSI_Position_Generator:
         # Initialise one RSI object for each stock
         self.rsi_obj = RSI_Position_Generator()
         self.rsi_history = []
+
+        # Initialise boolean reflecting if we are currently entered into a position
+        self.has_entered_position = False
+        # Declare boolean reflecting what position we are entered in
+        self.position_is_long = False
+        # Initialise a stop-loss
+        self.stop_loss = 0
+        # Declare variable to store price of band on day we entered the trade
+        self.band_price = 0
+        # Initialise variable that stores the number of shares held for this stock
+        self.num_shares_held = 0
+
         # Counter to track how many days has passed, since day does not always start from 0.
         self.days_passed = 0
         # NOTE: these lists are not necessary if we are only considering values from
@@ -61,20 +73,60 @@ class Bollinger_RSI_Position_Generator:
             self.upper_band_ls.append(sma + self.multiplier * std)
             self.lower_band_ls.append(sma - self.multiplier * std)
 
-            # Check if stock price is trading around the upper/lower bands,
-            # confirm with RSI value.
-            if (
-                price_history[-1] >= self.upper_band_ls[-1] and
-                self.rsi_history[-1] > 70
-            ):
-                self.cash_equiv = -10000
-            elif (
-                price_history[-1] <= self.lower_band_ls[-1] and
-                self.rsi_history[-1] < 30
-            ):
-                self.cash_equiv = 10000
+            # Set stop-loss to be half the difference between the bands and the SMA
+            # Note that the take-profit will just be the price of the SMA.
+            stop_loss_band_diff = (self.multiplier * std) / 2
+
+            # Check whether we have previously entered or exited a position.
+            # If we have entered a position, check if price has met stop-loss or
+            # take-profit.
+            # If not, see if we should enter a position based on Bollinger.
+            if self.has_entered_position and self.position_is_long:
+                # We were already buying on a previous day
+                if (
+                    price_history[-1] <= self.band_price - stop_loss_band_diff or
+                    price_history[-1] >= sma
+                ):
+                    # Exit if price drops below stop-loss to reduce losses, or cash out
+                    # if price rises above take-profit.
+                    self.cash_equiv -= (self.num_shares_held * price_history[-1])
+                    self.has_entered_position = False
+                    self.num_shares_held = 0
+            elif self.has_entered_position:
+                # We were already shorting on some previous day
+                if (
+                    price_history[-1] >= self.band_price + stop_loss_band_diff or
+                    price_history[-1] <= sma
+                ):
+                    # Exit if price rises above stop-loss to reduce losses, or cash out
+                    # if price falls below take-profit.
+                    self.cash_equiv += (self.num_shares_held * price_history[-1])
+                    self.has_entered_position = False
+                    self.num_shares_held = 0
+            else:
+                ### Not entered in any trade (positionless) ###
+                # Check if stock price is trading around the upper/lower bands,
+                # confirm with RSI value.
+                if (
+                    price_history[-1] >= self.upper_band_ls[-1] and
+                    self.rsi_history[-1] > 70
+                ):
+                    self.cash_equiv -= 10000
+                    self.has_entered_position = True
+                    self.position_is_long = False
+                    self.band_price = self.upper_band_ls[-1]
+                    self.num_shares_held = self.cash_equiv // price_history[-1]
+                elif (
+                    price_history[-1] <= self.lower_band_ls[-1] and
+                    self.rsi_history[-1] < 30
+                ):
+                    self.cash_equiv += 10000
+                    self.has_entered_position = True
+                    self.position_is_long = True
+                    self.band_price = self.lower_band_ls[-1]
+                    self.num_shares_held = self.cash_equiv // price_history[-1]
         
-        return self.cash_equiv // price_history[-1]
+        return self.num_shares_held
 
     # Gets the average of the last n days of price_history and returns it
     # (n is duration)
